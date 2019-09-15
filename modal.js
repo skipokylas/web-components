220779be4ff567cfc43e3c48b7c731906d3bb9a6
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-09-15 14:56:45 +0300

    added template for modal component

---
+++ modal.js
@@ -0,0 +1,72 @@
+class Modal extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                header {
+                    padding: 1rem;
+                }
+
+                header h1 {
+                    font-size: 1.25rem;
+                }
+
+                #backdrop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100vh;
+                    background: rgba(0,0,0,0.75);
+                    z-index: 10;
+                }
+
+                #modal {
+                    z-index: 100;
+                    position: fixed;
+                    top: 15vh;
+                    left: 25%;
+                    width: 50%;
+                    background: white;
+                    border-radius: 3px;
+                    box-shadow: 0 2px 8px rgba(0,0,0,0.25);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                }
+
+                #main {
+                    padding: 1rem;
+                }
+
+                #actions {
+                    border-top: 1px solid #ccc;
+                    padding: 1rem;
+                    display: flex;
+                    justify-content: flex-end;
+                }
+
+                #actions button {
+                    margin: 0 .25rem;
+                }
+            </style>
+            <div id="backdrop"></div>
+            <div id="modal">
+                <header>
+                    <h1>Please Confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>Ok</button>
+                </section>
+            </div>
+        `;
+    }
+
+}
+
+customElements.define('sk-modal', Modal)
